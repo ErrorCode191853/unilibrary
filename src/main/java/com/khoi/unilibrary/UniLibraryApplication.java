80bb6509103ac2baed156d3d2c0dc322515f71df
ErrorCode191853 <khoinguyenhuu20@gmail.com>
2024-08-23 14:35:46 +0700

    remove database migration

--- src/main/java/com/khoi/unilibrary/UniLibraryApplication.java
+++ src/main/java/com/khoi/unilibrary/UniLibraryApplication.java
@@ -2,8 +2,6 @@ package com.khoi.unilibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
